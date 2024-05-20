74ccd6e2d1ae73b08fa2ac386e6869c0d600e0f9
axkl20 <115080818+axkl20@users.noreply.github.com>
2024-05-20 20:26:29 +0200

    Update test_solution.py

--- backend/test/unit/test_solution.py
+++ backend/test/unit/test_solution.py
@@ -1,6 +1,13 @@
 import pytest
+import unittest.mock as mock
+
+from src.controllers.recipecontroller import RecipeController
 
 # add your test case implementation here
 @pytest.mark.unit
-def test():
-    pass
+def test_normal_recipe(diet = "NORMAL", take_best = True, expected = "Pancakes"):
+    mocked_recipecontroller = mock.MagicMock()
+    mocked_recipecontroller.find.return_value = "Pancakes"
+    sut = RecipeController(mocked_recipecontroller)
+    result = sut.get_recipe(diet, take_best)
+    assert result[] == expected
